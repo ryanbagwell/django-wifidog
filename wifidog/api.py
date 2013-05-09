98007ffe6b84508041e9d1e6db2658c14cc7bd64
Ryan Bagwell <ryan@ryanbagwell.com>
2013-05-08 22:37:58 -0500

    adding session authentication

--- wifidog/api.py
+++ wifidog/api.py
@@ -1,6 +1,8 @@
 from tastypie.resources import ModelResource
 from .models import *
 from .serializers import *
+from tastypie.authorization import DjangoAuthorization
+from tastypie.authentication import SessionAuthentication
 
 
 class SystemStatusResource(ModelResource):
@@ -9,3 +11,5 @@ class SystemStatusResource(ModelResource):
         queryset = WIFIPing.objects.order_by('-ping_time')[:2]
         resource_name = 'status'
         serializer = SystemSatusSerializer()
+        authorization = DjangoAuthorization()
+        authentication = SessionAuthentication()
